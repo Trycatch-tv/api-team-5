f05000f80a13f912d0fb75303282fefd45d92859
Mateo Agudelo <mateobedoyaagudelo@gmail.com>
2023-04-09 14:42:07 -0500

    Listo el middleware para la verificación de usuarios administradores.

--- middlewares/verify.token.js
+++ middlewares/verify.token.js
@@ -1,7 +1,25 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-    const token = req.header('token');
+    const token = req.headers['access-token'];
+
+    if (!token) {
+        return res.status(403).send('¡El token es requerido para realizar esta petición!');
+    }
+
+    try {
+        const decoded = jwt.verify(token, 'signal');
+        req.user = decoded;
+
+        if(decoded.is_admin) {
+            return next();
+        } else {
+            return res.status(403).send('¡No tienes suficientes permisos para acceder aquí!');
+        }
+
+    } catch (errors) {
+        return res.status(401).send('¡Token incorrecto!');
+    }
 }
 
 module.exports = verifyToken;
